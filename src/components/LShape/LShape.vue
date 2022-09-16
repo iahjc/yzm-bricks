86d7949242e502bb05c577456a29be4c92f2df4d
alines <hanruigang@163.com>
2022-09-16 15:00:47 +0800

    init project

---
+++ src/components/LShape/LShape.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :style="styleProps"
+    class="l-shape-component"
+    @click.prevent="handleclick"
+    ></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+import useComponentCommon from "../../hooks/useComponentCommon"
+import {transformToComponentProps, shapeDefaultProps, shapeStylePropsNames} from "../../defaultProps"
+const defaultProps = transformToComponentProps(shapeDefaultProps)
+
+export default defineComponent({
+  name: 'l-shape',
+  props: {
+    ...defaultProps
+  },
+  setup (props) {
+    const { styleProps, handleClick } = useComponentCommon(props, shapeStylePropsNames)
+    return {
+      styleProps,
+      handleClick
+    }
+  }
+})
+</script>
